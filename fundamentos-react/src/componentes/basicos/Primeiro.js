b9bafb86179251b0e7aceb8dcca986796fa22b34
manoelmachado <m.machado49@hotmail.com>
2023-01-27 17:08:36 -0300

    Update Primeiro.js

--- fundamentos-react/src/componentes/basicos/Primeiro.js
+++ fundamentos-react/src/componentes/basicos/Primeiro.js
@@ -1,3 +1,9 @@
 export default function Primeiro() {
-    return 'Primeiro componente'
+    const msg = 'Seja bem-vindo(a)'
+    return (
+        <div>
+            <p>{ msg }</p>
+            <h2>testando JSX</h2>
+        </div>
+    )
 }
