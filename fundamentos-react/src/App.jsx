41e0abac0d8b3ee9ee028a10e9347a07fbfb6fdc
manoelmachado <m.machado49@hotmail.com>
2023-01-31 17:18:22 -0300

    apenas configurando o prettier
    apenas configurando o prettier

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import Primeiro from './componentes/basicos/Primeiro'
-import ComParametro from './componentes/basicos/ComParametro'
-import Fragmento from './componentes/basicos/Fragmento'
+import Primeiro from "./componentes/basicos/Primeiro";
+import ComParametro from "./componentes/basicos/ComParametro";
+import Fragmento from "./componentes/basicos/Fragmento";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default _ => 
-    <div id="app">
-        <h1>Fundamentos React!</h1>
-        <p>Aprendendo a adicionar Arrow Function e diminuindo nossa função</p>
-        <hr />
-        <Fragmento />
-        <hr />
-        <ComParametro 
-            titulo="Situação do aluno:"
-            aluno="Pedro Silva"
-            nota={9.3} />
-        <ComParametro 
-            titulo="Situação do aluno:"
-            aluno="Maria"
-            nota={6.7} />
-        <hr />
-        <Primeiro></Primeiro>
-    </div>
+export default (_) => (
+  <div id="app">
+    <h1>Fundamentos React!</h1>
+    <p>Aprendendo a adicionar Arrow Function e diminuindo nossa função</p>
+    <hr />
+    <Fragmento />
+    <hr />
+    <ComParametro titulo="Situação do aluno:" aluno="Pedro Silva" nota={9.3} />
+    <ComParametro titulo="Situação do aluno:" aluno="Maria" nota={6.7} />
+    <hr />
+    <Primeiro></Primeiro>
+  </div>
+);
