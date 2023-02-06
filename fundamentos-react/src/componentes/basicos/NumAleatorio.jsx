6fdd2331ac4e1ff031c4788bc8e8cdbca17398cf
manoelmachado <m.machado49@hotmail.com>
2023-02-06 12:50:11 -0300

    finalizando exercício

--- fundamentos-react/src/componentes/basicos/NumAleatorio.jsx
+++ fundamentos-react/src/componentes/basicos/NumAleatorio.jsx
@@ -1,3 +1,12 @@
-export default function numAleatorio(min, max) {  
-    return Math.floor((Math.random() * (min + max)))
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    const aleatorio = parseInt(Math.random() * (props.max - props.min)) + props.min
+    return (
+    <div>
+        <h2>Valor Aleatório</h2>
+        <p><strong>Valor mínimo:</strong> { props.min }</p>
+        <p><strong>Valor máximo:</strong> { props.max }</p>
+        <p><strong>Valor escolhido:</strong> { aleatorio }</p>
+    </div>
+    )
 }
