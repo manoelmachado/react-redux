1e9fc7fca35d913767a21540d049fc6de4d06f25
manoelmachado <m.machado49@hotmail.com>
2023-02-06 17:29:31 -0300

    Update Card.jsx

--- fundamentos-react/src/componentes/layout/Card.jsx
+++ fundamentos-react/src/componentes/layout/Card.jsx
@@ -1,6 +1,7 @@
 import "./Card.css"
 import React from "react";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (props) => {
   return (
     <div className="Card">
