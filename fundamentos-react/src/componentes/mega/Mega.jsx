4c433fcbadd08843cb2ddd0bfa66e54cf4164c90
manoelmachado <m.machado49@hotmail.com>
2023-02-27 11:58:52 -0300

    desafio megasena
    gerando números aleatórios que não se repetem em um array

--- fundamentos-react/src/componentes/mega/Mega.jsx
+++ fundamentos-react/src/componentes/mega/Mega.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
 export default (props) => {
-  
   return (
     <div>
       <button>gerar</button>
     </div>
   );
 };
+
+function gerarNumeroNaoContido(min, max, array) {
+  const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min;
+  return array.includes(aleatorio)
+    ? gerarNumeroNaoContido(min, max, array)
+    : aleatorio;
+}
+
+function gerarNumeros(qtde) {
+  const numeros = Array(qtde)
+    .fill(0)
+    .reduce((nums) => {
+      const novoNumero = gerarNumeroNaoContido(1, 60, nums);
+      return [...nums, novoNumero];
+    }, [])
+    .sort((n1, n2) => n1 - n2);
+
+  return numeros;
+}
