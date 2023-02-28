4cc56d4093dc2c78db33d69ec2680f9d00cabe33
manoelmachado <m.machado49@hotmail.com>
2023-02-28 17:38:57 -0300

    botões calculadora
    adicionando o estilo dos botões na calculadora e adicionando também as funções dos botões

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./Button.css";
 
 export default (props) => {
-  return <button className="button">{props.label}</button>;
+  let classes = "button ";
+  classes += props.operation ? "operation" : "";
+  classes += props.double ? "double" : "";
+  classes += props.triple ? "triple" : "";
+
+  return (
+    <button 
+
+      onClick={e => props.Click && props.Click(props.label)}
+      className={classes}>    
+      {props.label}
+    </button>
+  )
 };
