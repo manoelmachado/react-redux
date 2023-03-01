d31c614ca90b80c734c445e55ad70b8015521740
manoelmachado <m.machado49@hotmail.com>
2023-03-01 15:52:36 -0300

    finalizando a calculadora
    adicionando o restante da função de operação

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -26,7 +26,27 @@ export default class Calculator extends Component {
   }
 
   setOperation(operation) {
-    return console.log(operation);
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      });
+    }
   }
 
   addDigit(n) {
@@ -39,12 +59,12 @@ export default class Calculator extends Component {
     const displayValue = currentValue + n;
     this.setState({ displayValue, clearDisplay: false });
 
-    if (n !== '.') {
-      const i = this.state.current
-      const newValue = parseFloat(displayValue)
-      const values = [ ...this.state.values ]
-      values[i] = newValue
-      this.setState({ values })
+    if (n !== ".") {
+      const i = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+      values[i] = newValue;
+      this.setState({ values });
     }
   }
 
