2d72a8e936002e8e8a680bc066eb8091c036cbe1
manoelmachado <m.machado49@hotmail.com>
2023-03-01 13:05:18 -0300

    terminando funções da calculadora
    adicionando as funções da calculadora aos botões

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import "./Button.css";
 
 export default (props) => {
-  let classes = "button ";
-  classes += props.operation ? "operation" : "";
-  classes += props.double ? "double" : "";
-  classes += props.triple ? "triple" : "";
+  let classes = 'button ';
+  classes += props.operation ? 'operation' : '';
+  classes += props.double ? 'double' : '';
+  classes += props.triple ? 'triple' : '';
 
   return (
-    <button 
-
-      onClick={e => props.Click && props.Click(props.label)}
+    <button
+      onClick={_ => props.click && props.click(props.label)}
       className={classes}>    
       {props.label}
     </button>
