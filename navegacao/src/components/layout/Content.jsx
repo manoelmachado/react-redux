a1cb275416ee2bee443e46c7bdb433d66f73ddf3
manoelmachado <m.machado49@hotmail.com>
2023-03-03 10:55:27 -0300

    adicionando outros componentes
    adicionado componente Param.jsx e criado comunicação com o Home, About e Params, utilizando o Routes, Route e Link to

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,11 +1,18 @@
-import './Content.css'
-import React from 'react' 
+import "./Content.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-const Content = props => (
+import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
+import Param from "../../views/examples/Param";
+
+const Content = (props) => 
     <main className="Content">
-        Componente Content
-    </main>
+        <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/param/:id" element={<Param />} />
+        </Routes>
+    </main>;
 
-)
-
-export default Content
+export default Content;
--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -1,15 +1,23 @@
 import "./Menu.css";
 import React from "react";
 
+import { Link } from 'react-router-dom'
+
 const Menu = (props) => (
   <aside className="Menu">
     <nav>
       <ul>
         <li>
-            <a href="/">Início</a>
+            <Link to="/Home">Início</Link>
         </li>
         <li>
-            <a href="/">Sobre</a>
+            <Link to="/Param/123">Param #01</Link>
+        </li>
+        <li>
+            <Link to="/Param/legal!">Param #02</Link>
+        </li>
+        <li>
+            <Link to="/About">Sobre</Link>
         </li>
       </ul>
     </nav>
