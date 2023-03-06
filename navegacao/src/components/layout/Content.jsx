49f7e29d6e273ae08f80ab8b0561e74df3a56d88
manoelmachado <m.machado49@hotmail.com>
2023-03-06 12:25:50 -0300

    página 404
    complementando o exercício anterior, adicionamos uma página de erro em nossa aplicação de navegação

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../../views/examples/Home";
 import About from "../../views/examples/About";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => 
     <main className="Content">
@@ -12,7 +13,7 @@ const Content = (props) =>
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/param/:id" element={<Param />} />
-            
+            <Route path="/*" element={<NotFound />} />
         </Routes>
     </main>;
 
