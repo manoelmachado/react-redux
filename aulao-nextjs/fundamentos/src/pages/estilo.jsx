fad835b85bf4faea8ab9ed703e67d70af996a8e9
manoelmachado <m.machado49@hotmail.com>
2023-03-23 10:05:44 -0300

    relembrando como usar CSS em módulo

---
+++ aulao-nextjs/fundamentos/src/pages/estilo.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/estilo.module.css'
+
+export default (props) => {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+};
