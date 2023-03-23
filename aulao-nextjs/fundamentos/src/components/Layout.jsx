dd6b0eee6c06f45c248c14bcad18d9ad83f3dcb8
manoelmachado <m.machado49@hotmail.com>
2023-03-23 16:43:10 -0300

    apenas ajustando a indentação

--- aulao-nextjs/fundamentos/src/components/Layout.jsx
+++ aulao-nextjs/fundamentos/src/components/Layout.jsx
@@ -5,12 +5,10 @@ export default function Layout(props) {
   return (
     <div className={styles.layout}>
       <div className={styles.cabecalho}>
-        <h1>{props.titulo ?? 'Mais um exemplo'}</h1>
+        <h1>{props.titulo ?? "Mais um exemplo"}</h1>
         <Link href="/">Voltar</Link>
       </div>
-      <div className={styles.conteudo}>
-        {props.children}
-      </div>
+      <div className={styles.conteudo}>{props.children}</div>
     </div>
   );
 }
