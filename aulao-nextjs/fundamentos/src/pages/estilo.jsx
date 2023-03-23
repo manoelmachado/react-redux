5b9210c8ca2b9b7da06b41a91deaff0af8dae8dc
manoelmachado <m.machado49@hotmail.com>
2023-03-23 16:42:21 -0300

    adicionando exercícios de uma aula utilizando CSS modularizado

--- aulao-nextjs/fundamentos/src/pages/estilo.jsx
+++ aulao-nextjs/fundamentos/src/pages/estilo.jsx
@@ -1,11 +1,13 @@
-import styles from '../styles/estilo.module.css'
-import Link from 'next/link'
+import styles from "../styles/estilo.module.css";
+import Layout from "../components/Layout";
+import Link from "next/link";
 
 export default (props) => {
-    return (
-        <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
-            <h1>Estilo usando CSS Módulos</h1>
-        </div>
-    )
+  return (
+    <Layout titulo="Exemplo de CSS Modularizado">
+      <div className={styles.roxo}>
+        <h1>Estilo usando CSS Módulos</h1>
+      </div>
+    </Layout>
+  );
 };
