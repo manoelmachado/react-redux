5ba5773e2c028b15daddd80d6208681641df23c5
manoelmachado <m.machado49@hotmail.com>
2023-03-24 12:41:47 -0300

    adicionando um componente de navegação

--- aulao-nextjs/fundamentos/src/pages/index.tsx
+++ aulao-nextjs/fundamentos/src/pages/index.tsx
@@ -1,9 +1,18 @@
-import Link from "next/link";
+import Navegador from "../components/Navegador";
 
 export default function Inicio() {
   return (
-    <div>
-      <Link href="/estilo">Estilo</Link>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexWrap: "wrap",
+      }}>
+      <Navegador texto="Estilo" destino="/estilo" />
+      <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3" />
+      <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
     </div>
   );
 }
