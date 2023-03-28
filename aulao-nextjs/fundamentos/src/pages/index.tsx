39a59d908fff190471d855cfa68295fb7a9d45af
manoelmachado <m.machado49@hotmail.com>
2023-03-28 15:33:58 -0300

    ainda utilizando API
    pegando informações de API utilizando fetch e await

--- aulao-nextjs/fundamentos/src/pages/index.tsx
+++ aulao-nextjs/fundamentos/src/pages/index.tsx
@@ -17,6 +17,7 @@ export default function Inicio() {
       <Navegador texto="Navegação #01" destino="/navegacao" cor="green" />
       <Navegador texto="Navegação #02" destino="/cliente/sp-02/321" cor="blue"/>
       <Navegador texto="Componente com Estado" destino="/estado" cor="gray" />
+      <Navegador texto="Integração com API #01" destino="/integracao_1" cor="purple" />
     </div>
   );
 }
