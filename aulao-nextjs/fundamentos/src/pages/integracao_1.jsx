24d6598e28d97d8839af5b77169175804bedbd00
manoelmachado <m.machado49@hotmail.com>
2023-03-29 15:20:54 -0300

    Update integracao_1.jsx

--- aulao-nextjs/fundamentos/src/pages/integracao_1.jsx
+++ aulao-nextjs/fundamentos/src/pages/integracao_1.jsx
@@ -1,32 +1,34 @@
-import Layout from '../components/Layout'
-import { useState } from 'react'
+import Layout from "../components/Layout";
+import { useState } from "react";
 export default function Integracao() {
+  const [cliente, setCliente] = useState({});
+  const [codigo, setCodigo] = useState(1);
 
-    const [cliente, setCliente] = useState({})
-    const [codigo, setCodigo] = useState(1)
+  async function obterCliente() {
+    const resp = await fetch(`http://localhost:3000/api/clientes/${codigo}`);
+    const dados = await resp.json();
+    setCliente(dados);
 
-    async function obterCliente() {
-        const resp = await fetch(`http://localhost:3000/api/clientes/${codigo}`)
-        const dados = await resp.json()
-        setCliente(dados)
+    // fetch(`http://localhost:3000/api/clientes/${codigo}`)
+    //     .then(resp => resp.json())
+    //     .then(dados => setCliente(dados))
+  }
 
-        // fetch(`http://localhost:3000/api/clientes/${codigo}`)
-        //     .then(resp => resp.json())
-        //     .then(dados => setCliente(dados))
-    }
-
-    return (
-        <Layout>
-            <div>
-                <input type="number" value={codigo}
-                onChange={e => setCodigo(e.target.value)}/>
-                <button onClick={obterCliente}>Obter Cliente</button>
-            </div>
-            <ul>
-                <li>Código: {cliente.id} </li>
-                <li>Nome: {cliente.nome} </li>
-                <li>Email: {cliente.email} </li>
-            </ul>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <div>
+        <input
+          type="number"
+          value={codigo}
+          onChange={(e) => setCodigo(e.target.value)}
+        />
+        <button onClick={obterCliente}>Obter Cliente</button>
+      </div>
+      <ul>
+        <li>Código: {cliente.id} </li>
+        <li>Nome: {cliente.nome} </li>
+        <li>Email: {cliente.email} </li>
+      </ul>
+    </Layout>
+  );
 }
