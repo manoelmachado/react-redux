115f47ce834c4dbed7e3b6a596e0d4cc5546c021
manoelmachado <m.machado49@hotmail.com>
2023-04-03 12:52:50 -0300

    adicionando estilos css com tailwind

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Layout from "../../components/Layout";
+
 export default function Home() {
   return (
-    <div className={``}>
-     
+    <div
+      className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      bg-white
+    `}>
+      <Layout titulo="Cadastro simples">
+        <span>Conteúdo</span>
+      </Layout>
     </div>
   );
 }
