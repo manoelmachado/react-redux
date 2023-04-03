37ccd1fa5e312420bfebcf1936050058770bb9cd
manoelmachado <m.machado49@hotmail.com>
2023-04-03 16:16:30 -0300

    adicionando outro componente de Tabela
    adicionando o componente Tabela e adicionando também funções para renderizar melhor a página

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -1,6 +1,16 @@
 import Layout from "../../components/Layout";
+import Tabela from "../../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
+
   return (
     <div
       className={`
@@ -9,7 +19,7 @@ export default function Home() {
       bg-white
     `}>
       <Layout titulo="Cadastro simples">
-        <span>Conteúdo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   );
